845b3d53c00bae195466131ce741aa0601309d5d
Nirav-TecEX <103412279+Nirav-TecEX@users.noreply.github.com>
2022-07-25 11:58:39 +0200

    tested coverage.py and updated readme

--- run_test.py
+++ run_test.py
@@ -1,7 +1,9 @@
 import json
+import logging
 from logging.config import dictConfig
 import time
 import os
+import sys
 
 from testrun.testengine import RulesEngineTester
 from git_api.misc import update_files
@@ -11,26 +13,71 @@ __PATHS = {'temp_folder': os.path.join(os.getcwd(), "temp"),
             'excel_files': os.path.join(os.getcwd(), "temp", "excel_files"),
             'jsons': os.path.join(os.getcwd(), "temp", "excel_files", "jsons")}
 
+
+def make_folders():
+    for key in __PATHS.keys():
+        if not os.path.exists(__PATHS[key]):
+            os.mkdir(__PATHS[key])
+
+
+def filter_args(args):
+    filtered_args = {}
+    try:
+        json_file_name = args[0]
+        filtered_args["json_file_name"] = json_file_name
+    except IndexError:
+        filtered_args["json_file_name"] = None
+    
+    return filtered_args
+
+
+def load_json_commands(json_file_name=None):
+    if json_file_name:
+        try:
+            with open(json_file_name) as f:
+                json_commands = json.load(f)
+        except FileNotFoundError:
+            print("""
+                    SELECTED file does not exist. Please use add a useable
+                    filename as an arg. """)
+    else:
+        try:
+            with open("JsonCommands2.json") as f:
+                json_commands = json.load(f)
+        except FileNotFoundError:
+            print("""
+                DEFAULT file does not exist. Please use add a useable
+                filename as an arg. """)
+
+    return json_commands
+
+
 if __name__ == "__main__":
     with open("logging_config.json") as f:
         log_config = json.load(f)
     dictConfig(log_config)
 
-    with open("JsonCommands2.json") as f:
-        json_commands = json.load(f)
+    __log = logging.getLogger("TestRun")
+
+    __log.info("Checking/Creating folder directory... ")
+    make_folders()
+
+    filtered_args = filter_args(sys.argv[1:])
+    json_file_name = filtered_args["json_file_name"]
+
+    __log.info(f"Using file {json_file_name}")
     
-    for key in __PATHS.keys():
-        if not os.path.exists(__PATHS[key]):
-            os.mkdir(__PATHS[key])
-
     try:
-        test_RE = RulesEngineTester(__PATHS['excel_files'])
-    except Exception as e:
-        raise "Unable to instantiate the RulesEngineTester class."
-    try:
-        test_RE.run(tests_to_run=json_commands)
-    except Exception as e:
-        raise f"Error during testrun. \n\t {e}"
+        json_commands = load_json_commands(json_file_name)
 
-    print("Tests Complete... \n Shutting down...")
+        try:
+            test_RE = RulesEngineTester(__PATHS['excel_files'])
+            test_RE.run(tests_to_run=json_commands)
+            print("Tests Complete... \n Shutting down...")
+        except Exception as e:
+            raise "Unable to instantiate the RulesEngineTester class."
+
+    except Exception as e:
+        __log.info(f"Error during process. Shutting down ...")
+    
     time.sleep(5)
